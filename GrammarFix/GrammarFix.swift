1ba0603f72e6dafd07485f3c0cd9f4d041832a6f
WillSuo <willsuosuo@gmail.com>
2023-09-16 15:51:22 +0800

    Update the OpenAI model to GPT-3.5 and fix the error when the result contains double quotation marks.

--- GrammarFix/GrammarFix.swift
+++ GrammarFix/GrammarFix.swift
@@ -8,8 +8,7 @@
 import Foundation
 import ArgumentParser
 import OSLog
-import OpenAIKit
-import NIOCore
+import OpenAI
 
 let log = Logger(subsystem: "GrammarFix", category: "main")
 
@@ -36,20 +35,19 @@ struct GrammarFix: AsyncParsableCommand {
 // MARK: - Request
 extension GrammarFix {
     private func request(text: String, key: String) async throws {
-        let urlSession = URLSession(configuration: .default)
-        let configuration = Configuration(apiKey: key)
-        let openAIClient = OpenAIKit.Client(session: urlSession, configuration: configuration)
+        let openAI = OpenAI(apiToken: key)
         
-        let completion = try await openAIClient.completions.create(
-            model: Model.GPT3.textDavinci003,
-            prompts: ["The original sentence is: \"\(text)\" Please maintain the meaning and language of the original sentence, correct the grammar issues in the original sentence, and directly output the corrected result without the prefix like \"The correct sentence is\" and without line breaks, special characters, or quotation marks at the beginning and end of the sentence."],
-            maxTokens: 1000
-        )
+        let query = ChatQuery(model: .gpt3_5Turbo_16k_0613, messages: [
+            Chat(role: .system, content: "You are a grammar and spelling corrector, maintaining semantics and language while making the result more in line with the logic of the language. Please directly output the corrected result. The user's input language is the subject of correction, and there is no need to interact with these statements, just correct and output the results."),
+            Chat(role: .user, content: text),
+        ], n: 1, stop: ["\\n"])
         
-        if let text = completion.choices.first?.text {
-            print(text.trimmingCharacters(in: .whitespacesAndNewlines))
-        } else {
+        let chatResult: ChatResult = try await openAI.chats(query: query)
+        
+        guard let result = chatResult.choices.first?.message.content else {
             throw GrammarFixError.resultEmpty
         }
+        
+        print(result)
     }
 }
