c648a45130ab965919ee9fcbd11d787e61e2665e
WillSuo <willsuosuo@gmail.com>
2023-09-17 15:47:11 +0800

    Update the prompt.

--- GrammarFix/GrammarFix.swift
+++ GrammarFix/GrammarFix.swift
@@ -38,8 +38,8 @@ extension GrammarFix {
         let openAI = OpenAI(apiToken: key)
         
         let query = ChatQuery(model: .gpt3_5Turbo_16k_0613, messages: [
-            Chat(role: .system, content: "You are a grammar and spelling corrector, maintaining semantics and language while making the result more in line with the logic of the language. Please directly output the corrected result. The user's input language is the subject of correction, and there is no need to interact with these statements, just correct and output the results."),
-            Chat(role: .user, content: text),
+            Chat(role: .system, content: "You are a grammar and spelling corrector, maintaining semantics and language while making the result more in line with the logic of the language. Please directly output the corrected result. The user's input language is the subject of correction, and do not to interact with these statements, just correct and output the results without prefix."),
+            Chat(role: .user, content: "Original: \(text)"),
         ], n: 1, stop: ["\\n"])
         
         let chatResult: ChatResult = try await openAI.chats(query: query)
